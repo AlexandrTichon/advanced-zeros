57d350db47ed605b71ac2aedeb803f145b1781cb
Alexandr Tichon <tsikhan.p@gmail.com>
2018-10-05 22:17:59 +0300

    Secound day of work

--- src/index.js
+++ src/index.js
@@ -1,33 +1,56 @@
-module.exports = function getZerosCount(number, base) {
-    let i = Math.floor(Math.log(number)/Math.log(base));
-    console.log(i);
-    console.log(number / Math.pow(base, i));
-    var zero_number = 0,
-        slag = 0,
-        denominator = 5,
-        power = 1;
-    while (true) {
-        denominator = Math.pow(5, power);
-        slag = Math.floor(number / denominator); 
-        if (slag < 1) break;
-        else zero_number += slag;
-        power++;
-    }
-    console.log(zero_number / i);
-    return zero_number;
-}
-function maxPowerOf(p, n)
-{
-    var k = 0, powerOfP = 1;
-    while (true)
-    {
-        powerOfP *= p;
-        var addend = Math.floor( n / powerOfP);
-        if (addend == 0)
-            return k;
-        k += addend;
+module.exports = function getZerosCountBase(number, base){
+    powers = primeFactors(base);
+    for (let i = 1; i <= 1+powers.split('*').length-1; i++){
+        console.log(getZerosCount(number, powers[powers.indexOf('^') - 1]));  
     }
 }
 
-console.log(maxPowerOf(5, 10) / 2);
-getZerosCount(93443454, 194);
+
+function getZerosCount(number, _denominator) {
+    var zero_number = 0,
+      slag = 0,
+      denominator = _denominator;
+      power = 1;
+  while (true) {
+      denominator = Math.pow(_denominator, power);
+      slag = Math.floor(number / denominator); 
+      if (slag < 1) break;
+      else zero_number += slag;
+      power++;
+    }
+    console.log(zero_number);
+    return zero_number;
+}
+
+
+function primeFactors(number){
+    var copy_number = number;
+    var answer_mass = [];
+    let i=2, iter = 0;
+    while (i <= number)
+    {
+        if (number % i == 0)
+        {
+            answer_mass[iter] = i;
+            number /= i;
+            iter++;
+        }
+        else i++;
+    }
+    console.log(answer_mass);
+    var answer = '';
+    for (let i = 0; i < answer_mass.length; i++) {
+        primeFactor = answer_mass[i];
+        var power = 1;
+        if (answer.length != 0) answer += '*';
+        for (let j = i + 1; j < answer_mass.length; j++)
+        {
+            if (answer_mass[j] === primeFactor) power++;
+            else break;
+        }
+        answer += primeFactor + '^' + power;
+        i += power - 1;
+    }
+    console.log(answer);
+    return answer;
+}
